6d745753536f042b473ddf24017ece6e255ac54e
Andrew Wichmann <andrewp.wichmann@gmail.com>
2024-05-04 21:28:33 -0400

    init commit

---
+++ cmd/tt/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+    tea "github.com/charmbraclet/bubbletea"
+)
+
+func main() {
+    fmt.Printf("Hello World\n")
+    fmt.Printf("h")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Andrew-Wichmann/typing-test
+
+go 1.22.2
